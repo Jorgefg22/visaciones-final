a397734df775cc2c8d1858781c0209aea0de93ee
jorgefg22 <jorgefernandezgonzales.22@gmail.com>
2023-11-09 18:07:51 -0400

    second part

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsComponent } from './pages/forms/forms.component';
+import { FormtwoComponent } from './pages/formtwo/formtwo.component';
+import { FormviewComponent } from './pages/formview/formview.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'/inicio',pathMatch:'full'},  
+  {path:'inicio',component:FormsComponent},
+  {path:'typevc',component:FormtwoComponent},
+  {path:'info',component:FormviewComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { FormsComponent } from './pages/forms/forms.component';
+import { FormtwoComponent } from './pages/formtwo/formtwo.component';
+import { FormviewComponent } from './pages/formview/formview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    FormsComponent,
+    FormtwoComponent,
+    FormviewComponent
   ],
   imports: [
     BrowserModule,

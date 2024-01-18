2dd536071c682cc08db1f37460f638479fd2ae00
jorgefg22 <jorgefernandezgonzales.22@gmail.com>
2024-01-18 10:41:02 -0400

    las

--- src/app/pages/formtwo/formtwo.component.ts
+++ src/app/pages/formtwo/formtwo.component.ts
@@ -1,46 +1,168 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, Injectable, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
-import {FormsComponent} from '../forms/forms.component'
+import { FormsComponent } from '../forms/forms.component'
+import { InmuebleComponent } from 'src/app/vs/inmueble/inmueble.component';
+ 
 
 @Component({
   selector: 'app-formtwo',
   templateUrl: './formtwo.component.html',
   styleUrls: ['./formtwo.component.css'],
-  
+
+})
+@Injectable({
+  providedIn: 'root'
 })
 export class FormtwoComponent {
   @ViewChild('miSelect') miSelect: ElementRef | undefined;
   @ViewChild('codCat') codCat: ElementRef | undefined;
   @ViewChild('nInm') nInm: ElementRef | undefined;
   @ViewChild('numero') numero: ElementRef | undefined;
-  constructor( private formsComponent:FormsComponent, private router: Router) { }
+  @ViewChild(InmuebleComponent) inmueble!: InmuebleComponent;
+ 
+  constructor( private formsComponent: FormsComponent, private router: Router) { 
+     
+  }
 
-  typeVisacion = '' ;
+  typeVisacion = '';
   codcat = false;
   ninm = false;
   Numero = '';
+  test = 'El valor se cambio';
+
+
+  //nombres       //apellidos       //documeno identificacion     // tipo de documento     //expedicion     // genero
+  nombre1 = " ";  apellido1 = "";    dni1 = "";                   tdoc = "";               exp1 = "";       gen1 ="";
+
+  //tipo contribuyente       //fecha nacimiento      //domicilio legal    //pmc         //estado civil 
+  tpct1 = "";               fechanac1 = "";          domleg1 = "";        pmc1 = "";    estciv1 = "";
+  //datos tecnicos
+  //Matricula computarizada   //asiento     //sup.Terreno/cat     //construcciones    //clase     //tipo      //material via
+  matcomp = "";               asiento = "";  supterrcat =" 12";         constr = "" ;       clas = "";   tipe = "" ; matvia = "";
   
+   //sup. Terreno/ruat   //construc. adicion.     //Area      //inclinacion        //servicios            //zona tributaria
+   supterruat = "";      constad = "";            area ="";    inclinacion = "" ;       servicios = "";     zntrib = ""
+  
+
+  gettest(){
+
+    return this.supterrcat
+  }
+
   obtenerValor() {
     if (this.miSelect && this.numero) {
       const valorSeleccionado = this.miSelect.nativeElement.value;
       this.typeVisacion = valorSeleccionado;
       this.Numero = this.numero.nativeElement.value;
       console.log(this.Numero);
-      if(this.codCat && this.codCat.nativeElement.checked){
+      if (this.codCat && this.codCat.nativeElement.checked) {
         this.codcat = true;
         console.log(this.codcat)
-      }else if(this.nInm && this.nInm.nativeElement.checked){
+      } else if (this.nInm && this.nInm.nativeElement.checked) {
         this.ninm = true;
         console.log(this.ninm)
+       
       }
 
       if (this.Numero == "411740" || this.Numero == "01-042-00025-000") {
-        
-      }
-      
+       
+        this.nombre1 = "MARTIN";
+        this.apellido1 = "ROQUE TINTAYA";
+        this.dni1 = "2359988";
+        this.tdoc = "CEDULA DE IDENTIDAD" ; 
+        this.tpct1 = "NATURAL";
+        this.fechanac1 = "03/11/1957";
+        this.domleg1 = "CALLE INNOMINADA S/N, DISTRITO SACABA";
+        this.pmc1 = "2359988019";
+        this.exp1 = "LA PAZ";
+        this.estciv1 = "SOLTERO";
+        this.gen1 = "MASCULINO";
+        this.matcomp = "3.10.101.0013099";
+        this.asiento = "A-4";
+        this.supterrcat = "113";
+        this.constr = "1";
+        this.clas = "VIVIENDA UNIFAMILIAR";
+        this.tipe = "UNICA";
+        this.matvia = "CEMENTO";
+        this.supterruat = "113";
+        this.constad = "1";
+        this.area = "URBANO";
+        this.inclinacion = "PLANO";
+        this.servicios = "AGUA, LUZ,TELEFONO, ALCANTARILLADO";
+        this.zntrib = "ZONA2";
 
+        console.log(this.supterrcat)
+
+        this.router.navigateByUrl('/inmueble')
+       
+
+      } else if (this.Numero == "392649" || this.Numero == "03-018-00009-000") {
+
+       
+        this.nombre1 = "JUAN";
+        this.apellido1 = "GUTIERREZ ESPINOZA";
+        this.dni1 = "951500";
+        this.tdoc = "CEDULA DE IDENTIDAD" ; 
+        this.tpct1 = "NATURAL";
+        this.fechanac1 = "27/01/1942";
+        this.domleg1 = "CALLE AROMA #6, DISTRITO 1 SACABA (URBANA)";
+        this.pmc1 = "961500016";
+        this.exp1 = "COCHABAMBA";
+        this.estciv1 = "VIUDO(A)";
+        this.gen1 = "MASCULINO";
+        this.matcomp = "3.10.1.01.0054807";
+        this.asiento = "A-10";
+        this.supterrcat = "1705";
+        this.constr = "0";
+        this.clas = "TERRENO";
+        this.tipe = "UNICA";
+        this.matvia = "PIEDRA";
+        this.supterruat = "1705";
+        this.constad = "0";
+        this.area = "URBANO";
+        this.inclinacion = "PLANO";
+        this.servicios = "AGUA, LUZ, TELEFONO, ALCANTARILLADO";
+        this.zntrib = "ZONA3";
+
+
+        this.router.navigateByUrl('/inmueble')
+
+      } else if (this.Numero == "389651" || this.Numero == "07-066-00022-000") {
+        this.nombre1 = "MARIA";
+        this.apellido1 = "PACCI CANAZA Vda. de HUANCA";
+        this.dni1 = "3129066";
+        this.tdoc = "CEDULA DE IDENTIDAD" ; 
+        this.tpct1 = "NATURAL";
+        this.fechanac1 = "14/09/1938";
+        this.domleg1 = "CALLE INNOMINADA S/N, DISTRITO LINDE";
+        this.pmc1 = "3129066016";
+        this.exp1 = "COCHABAMBA";
+        this.estciv1 = "VIUDO (A)";
+        this.gen1 = "FEMENINO";
+        this.matcomp = "3.10.1.01.0054807";
+        this.asiento = "A-4";
+        this.supterrcat = "1962";
+        this.constr = "1";
+        this.clas = "VIVIENDA UNIFAMILIAR";
+        this.tipe = "UNIA";
+        this.matvia = "AZFALTO";
+        this.supterruat = "1962";
+        this.constad = "1";
+        this.area = "URBANO";
+        this.inclinacion = "PLANO";
+        this.servicios = "AGUA, LUZ, TELEFONO,ALCANTARILLADO";
+        this.zntrib = "ZONA5";
+        this.router.navigateByUrl('/inmueble')
+
+      }
 
     }
   }
 
+  asignarValores(){
+   
+   
+    this.inmueble.updateinformation(this.Numero,this.matcomp,this.asiento,this.supterrcat,this.constr,this.supterruat,this.constad,this.tipe, this.area,this.inclinacion,this.servicios)
+    
+  }
 }
--- src/app/vs/inmueble/inmueble.component.ts
+++ src/app/vs/inmueble/inmueble.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Injectable, OnInit, ViewChild } from '@angular/core';
 import {Map, marker, polygon, tileLayer } from 'leaflet';
 import { Router } from '@angular/router';
+import { FormtwoComponent } from 'src/app/pages/formtwo/formtwo.component';
 
 
 @Component({
@@ -9,10 +10,54 @@ import { Router } from '@angular/router';
   styleUrls: ['./inmueble.component.css']
 })
 
-export class InmuebleComponent {
+@Injectable({
+  providedIn: 'root'
+})
+
+export class InmuebleComponent implements OnInit {
+  static updateinformation(Numero: string, matcomp: string, asiento: string, supterrcat: string, constr: string, supterruat: string, constad: string, tipe: string, area: string, inclinacion: string, servicios: string) {
+    throw new Error('Method not implemented.');
+  }
+   
+  @ViewChild('codcat', { static: true }) codcat!: ElementRef;
+  @ViewChild('matcomp', { static: true }) matcomp!: ElementRef;
+  @ViewChild('asiento', { static: true }) asiento!: ElementRef;
+  @ViewChild('suptercat', { static: true }) suptercat!: ElementRef;
+  @ViewChild('constr', { static: true }) constr!: ElementRef;
+  @ViewChild('supteruat', { static: true }) supteruat!: ElementRef;
+  @ViewChild('constrat', { static: true }) constrat!: ElementRef;
+  @ViewChild('tipo', { static: true }) tipo!: ElementRef;
+  @ViewChild('area', { static: true }) area!: ElementRef;
+  @ViewChild('incli', { static: true }) incli!: ElementRef;
+  @ViewChild('serv', { static: true }) serv!: ElementRef;
+  @ViewChild(FormtwoComponent) formtwo!: FormtwoComponent;
+  //@ViewChild('mylabel', { static: true })
+  //mylabel!: ElementRef;
+
+ 
+  constructor(private formTwo: FormtwoComponent,private router: Router) { }
+
+  ngOnInit(){
+    console.log(this.formTwo.gettest())
+    //this.updateinformation(this.formTwo.Numero,this.formTwo.matcomp,this.formTwo.asiento,this.formTwo.supterrcat,this.formTwo.constr,this.formTwo.supterruat,this.formTwo.constad,this.formTwo.tipe,this.formTwo.area, this.formTwo.inclinacion, this.formTwo.servicios);
+  }
 
 
-  constructor(private router: Router) { }
+  updateinformation(codigoCat:string, matcomp:string,asiento:string,suptercat:string, constru:string,supteruat:string, constrat:string, tipo:string,area:string,incli:string,serv:string){
+
+    
+    this.codcat.nativeElement.innerText = codigoCat; 
+    this.matcomp.nativeElement.innerText = matcomp;
+    this.asiento.nativeElement.innerText = asiento;
+    this.suptercat.nativeElement.innerText = suptercat;
+    this.constr.nativeElement.innerText = constru,
+    this.supteruat.nativeElement.innerText = supteruat;
+    this.constrat.nativeElement.innerText = constrat;
+    this.tipo.nativeElement.innerText = tipo;
+    this.area.nativeElement.innerText = area;
+    this.incli.nativeElement.innerText = incli;
+    this.serv.nativeElement.innerText = serv;
+  }
 
   redirecToPageinmueble(){
     this.router.navigateByUrl('/inmueble')
@@ -42,7 +87,8 @@ export class InmuebleComponent {
        [-17.403872311394846, -66.03938776261947]
      ]).addTo(map).bindPopup('<nav class="navbar navbar-expand-lg navbar-light  p-3" style="background-color: #08477b ;"><div class="container-fluid"><a class="navbar-brand" href="#"><img src="../../../assets/logos/logo-sacaba.png" alt=""></a></div></nav><div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel"><div class="carousel-inner"><div class="carousel-item active"><img src="../../../assets/img/teatro.jpg" class="d-block w-100" alt="..."></div><div class="carousel-item"><img src="../../../assets/img/boulebard.jpg" class="d-block w-100" alt="..."></div></div><button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev"><span class="carousel-control-prev-icon" aria-hidden="true"></span><span class="visually-hidden">Previous</span></button><button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next"><span class="carousel-control-next-icon" aria-hidden="true"></span><span class="visually-hidden">Next</span></button></div><div><p>El GAMS es una Entidad Territorial Autónoma Metropolitana que impulsa el desarrollo económico local, humano y territorial a través de la prestación de servicios públicos a la población urbana y rural para contribuir al vivir bien, basados en los principios de democracia y equidad de sus políticas públicas.</p></div>');
      
-     map.fitBounds([
-     ]);
+   //  map.fitBounds([]);
    }    
+   
+
 }
